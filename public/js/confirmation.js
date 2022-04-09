93cde08a05f1ff743c0e6f719ae1a831c9fb1c23
Prince Asiedu <princekofasiedu@gmail.com>
2022-04-09 09:14:15 +0000

    Changed file structure

---
+++ public/js/confirmation.js
@@ -0,0 +1,57 @@
+lastUrl = location.href;
+
+new MutationObserver(() => {
+  const url = location.href;
+  if (url !== lastUrl) {
+    lastUrl = url;
+    // onConfirmation();
+  }
+}).observe(document, { subtree: true, childList: true });
+
+function onConfirmation(item, replacement) {
+  console.log(verifyBtn);
+  chatData = JSON.parse(localStorage.getItem("chatData"));
+  console.log(chatData);
+
+  setInputValuesToChatData(chatData);
+  item.remove();
+  replacement.style.display = "block";
+}
+
+function setInputValuesToChatData(chatData) {
+  console.log(chatData);
+  // Get elements
+  confirmationInputs = document.querySelectorAll(".confirmationElem");
+  console.log(confirmationInputs);
+
+  for (let i = 0; i < confirmationInputs.length; i++) {
+    console.log(confirmationInputs[i]);
+    let name = confirmationInputs[i].getAttribute("name");
+
+    console.log(name);
+    if (name in chatData) {
+      confirmationInputs[i].setAttribute("value", `${chatData[name]}`);
+      console.log(confirmationInputs[i]);
+      console.log("Name in chatData!");
+    } else {
+      console.log("Name not found");
+    }
+
+    num = num + 1;
+    console.log(num)
+    // console.log(confirmationInputs)
+  }
+}
+
+function store() {
+  chatData = JSON.parse(localStorage.getItem("chatData"));
+
+  axios
+    .post("/post", chatData)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
