a7b3cd64cb6a53ca683a97a32fbaced272c6c717
Prince Kofi Asiedu <princekofasiedu@gmail.com>
2022-07-23 11:39:13 +0000

    First stage in connecting directly to sqlite database complete; next is connecting with web service

--- web/index.js
+++ web/index.js
@@ -1,45 +1,50 @@
 const fs = require("fs");
-// var http = require('http');
+const sql = require("sqlite3").verbose();
 
-// var nStatic = require('node-static');
+var bodyParser = require("body-parser");
 
-// var fileServer = new nStatic.Server('./public');
-
-// http.createServer(function (req, res) {
-//     fileServer.serve(req, res);
-
-//     if ( req.method == 'POST' ) {
-//         console.log(req.body)
-//     }
-
-// }).listen(5500);
-
-var bodyParser = require('body-parser');
-
-const express = require('express');
+const express = require("express");
+const { exit } = require("process");
 
 const app = express();
+const db = new sql.Database("../server2.db", (err) => {
+  if (err) {
+    console.log("Getting Error: ", err);
+    exit(1);
+  }
+} );
 
-// Parses the body for POST, PUT, DELETE, etc.
+db.serialize(() => {
+  db.run(`CREATE TABLE IF NOT EXISTS student_details (  
+        surname text not null,
+        firstname text not null,
+        other_names text
+
+  )`);
+
+  const stmt =
+    db.run(`INSERT INTO "student_details" (surname, firstname, other_names) 
+    VALUES('Prince', 'John', 'Doe')`);
+
+
+  db.each("SELECT * FROM student_details", (err, row) => {
+    console.log(row.firstname + ": " + row.surname);
+  });
+});
+
 app.use(bodyParser.json());
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/post', function(req, res, next){
+app.post("/post", function (req, res, next) {
+  console.log(req.body); // req.body contains the parsed body of the request.
 
-    console.log( req.body ); // req.body contains the parsed body of the request.
-    
+  const content = JSON.stringify( req.body );
+  console.log(content)
+  
+  // Adding data to databse goes here...
 
-    const content =JSON.stringify(req.body)
-
-    fs.appendFile("details.json", content, (err) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      //file written successfully
-    });
 
 });
 
-app.listen(8080, 'localhost');
+app.listen(8080, () => console.log("listening on localhost 8080"));
