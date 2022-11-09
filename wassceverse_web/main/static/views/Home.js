17ea0e1f8104308fc1cd18af051f87af91ca7eba
prince <princekofasiedu@gmail.com>
2022-11-09 15:47:13 +0000

    Added the school_selection page

--- wassceverse_web/main/static/views/Home.js
+++ wassceverse_web/main/static/views/Home.js
@@ -7,12 +7,39 @@ export default class extends AbstractView {
   }
 
   async getHTML() {
-    return `<header class="main-header">
-        <section>
-          <h1>WASSCEVERSE.COM</h1>
-          <p> Home paragraph1</p>
+      axios.get( "/api/schools" ).then( ( res ) => {
+          // -------- Get schools data --------
+          schools = res.data;
+          console.log( res.data );
+      } )
+    return `
+    <main id="main">
+      <section id="content">
+        <header class="content-header">
+          <h2>Select your school</h2>
+        </header>
+        <section id="school_selector">
+          <!-- The search bar goes here -->
+          <div class="search-bar">
+            <p>I am a</p>
+            <input type="text" id="search" oninput="inputChange(this)" />
+            <p>student</p>
+          </div>
+          <div class="results">
+            <!-- Search result descriptions -->
+            <div id="search_desc">
+              <span>Schools found: </span>
+              <span class="search_data_count">1</span>
+            </div>
+
+            <!-- Actual Search results -->
+            <div id="search_results">
+              <!-- The results appear here from main.js -->
+            </div>
+          </div>
         </section>
-      </header>
+      </section>
+    </main>
       `;
   }
 }
