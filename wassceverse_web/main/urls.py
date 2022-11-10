d70d3b0aba09cc10d772346fec729e7926a2060b
ZeroByte <65360444+bayaan-nasir@users.noreply.github.com>
2022-11-10 05:32:13 +0100

    Update

--- wassceverse_web/main/urls.py
+++ wassceverse_web/main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login)
+    path('', views.login, name="login"),
+    path('_/', views.conversation, name="conversation"),
+    path('*/', views.congrats, name="congrats")
 ]
--- wassceverse_web/main/views.py
+++ wassceverse_web/main/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def login(request):
     return render(request, 'login.html')
+
+def conversation(request):
+    return render(request, 'conversation.html')
+
+def congrats(request):
+    return render(request, 'congrats.html')
