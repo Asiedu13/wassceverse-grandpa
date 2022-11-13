2c3aab5cd3ff56a933c4460382b2b2741746b88b
ZeroByte <65360444+bayaan-nasir@users.noreply.github.com>
2022-11-14 00:56:30 +0100

    Update

--- wassceverse_web/main/views.py
+++ wassceverse_web/main/views.py
@@ -8,7 +8,10 @@ def login(request):
     if request.method == "POST":
         form = StudentCred(request.POST)
         if form.is_valid:
-            HttpResponseRedirect('conversation')
+            studentData = models.StudentDetails.objects.get(index_number = request.POST['index_number'])
+            if studentData.student_key == request.POST['key']:
+                HttpResponseRedirect('conversation')
+            
     else:
         form = StudentCred()
     context = {
