e27778f4242e7273a9042c2661ff85245078892f
prince <65360444+bayaan-nasir@users.noreply.github.com>
2022-11-18 06:28:07 +0000

    Update settings.py

--- wassceverse_web/wassceverse_web/settings.py
+++ wassceverse_web/wassceverse_web/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%gn!y+v+sh9!u56)qmy=oi0qeo^y!t%1x5y#zwqkw1bi@h^d6_'
+# SECRET_KEY = 'django-insecure-%gn!y+v+sh9!u56)qmy=oi0qeo^y!t%1x5y#zwqkw1bi@h^d6_'
+try:
+    SECRET_KEY = os.environ["SECRET_KEY"]
+except KeyError as e:
+    raise RuntimeError("Could not find a SECRET_KEY in environment") from e
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
